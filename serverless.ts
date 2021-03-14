6e70b6de281154ad7b9e2474df60f4fd94cf148a
MKortmann <companymvms@gmail.com>
2021-03-14 21:10:57 +0100

    first commit: following classes convert yaml to ts

---
+++ serverless.ts
@@ -0,0 +1,97 @@
+// import { DynamoDB } from 'aws-sdk';
+// import { table } from 'console';
+
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'udatype',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: '${opt:region, "eu-central-1"}',
+    stage: '${opt:stage, "dev"}',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      GROUPS_TABLE: 'Groups-${self:provider.stage}'
+    },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: [ "dynamodb:Scan", "dynamodb:PutItem"],
+        Resource: "arn:aws:dynamodb:${self:provider.region}:*:table/${self:provider.environment.GROUPS_TABLE}"
+      }
+    ]
+  },
+  functions: {
+    GetGroups: {
+      handler: 'src/lambda/http/getGroups.handler',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'groups',
+            cors: true
+          }
+        }
+      ]
+    },
+    createGroup: {
+      handler: 'src/lambda/http/createGroup.handler',
+      events: [
+        {
+          http: {
+            method: 'post',
+            path: 'groups',
+            cors: true
+          }
+        }
+      ]
+    }
+  },
+  resources: {
+    Resources: {
+       GroupsDynamoDBTable: {
+          Type: "AWS::DynamoDB::Table",
+          Properties: {
+             AttributeDefinitions: [
+                {
+                   "AttributeName": "id",
+                   "AttributeType": "S"
+                }
+             ],
+             KeySchema: [
+                {
+                   "AttributeName": "id",
+                   "KeyType": "HASH"
+                }
+             ],
+             BillingMode: "PAY_PER_REQUEST",
+             TableName: "${self:provider.environment.GROUPS_TABLE}"
+          }
+       }
+    }
+ }
+
+
+
+
+}
+
+module.exports = serverlessConfiguration;
---
+++ src/lambda/http/getGroups.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import 'source-map-support';
+import * as AWS from 'aws-sdk';
+
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const groupsTable = process.env.GROUPS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+
+  // Scan operation parameters
+  const scanParams = {
+    TableName: groupsTable,
+  }
+  console.log('Scan params: ', scanParams)
+
+  const result = await docClient.scan(scanParams).promise()
+
+  const items = result.Items
+
+  console.log('Result: ', result)
+
+  // Return result
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
+}
